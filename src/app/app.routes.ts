15a19e00eab843b56e01a010485704afe5368348
leonnn7 <leonnn7@github.com>
2026-03-10 15:19:13 +0100

    tasks app routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,13 @@ import {Routes} from '@angular/router';
 import {StartScreenPage} from "./pages/start-screen/start-screen-page";
 import {CameraPermissionsPage} from "./pages/permissions/camera-permissions/camera-permissions-page";
 import {LocationPermissionsPage} from "./pages/permissions/location-permissions/location-permissions-page";
-import {ShowTaskPage} from "./pages/show-task/show-task-page";
-import {TaskCompletedPage} from "./pages/task-completed/task-completed-page";
 import {ResultsPage} from "./pages/results/results-page";
 import {TabsPage} from "./pages/tabs/tabs.page";
 import {HomePage} from "./pages/home/home.page";
 import {SettingsPage} from "./pages/settings/settings.page";
 import {LeaderboardPage} from "./pages/leaderboard/leaderboard.page";
 import {ExplorePage} from "./pages/explore/explore.page";
+import {TasksPage} from "./pages/tasks/tasks.page";
 
 export const routes: Routes = [
   {path: '', redirectTo: 'start-screen', pathMatch: 'full'},
@@ -21,8 +20,6 @@ export const routes: Routes = [
       {path: 'location', component: LocationPermissionsPage}
     ]
   },
-  {path: 'show-task', component: ShowTaskPage},
-  {path: 'task-completed', component: TaskCompletedPage},
   {path: 'results', component: ResultsPage},
   {
     path: '',
@@ -33,6 +30,8 @@ export const routes: Routes = [
       {path: 'explore', component: ExplorePage},
       {path: 'leaderboard', component: LeaderboardPage},
       {path: 'settings', component: SettingsPage},
+      {path: 'tasks', redirectTo: 'tasks/1'},
+      {path: 'tasks/:id', component: TasksPage},
     ],
   },
 ];
